cb9d0dec48978cbaf05c51bc76bd3d6c713b6ac2
Sebastian W <sebastianmikael2000@gmail.com>
2023-10-06 13:38:53 +0200

    Fixed some shit and figured out assistant summoning

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,36 @@
 #include <Arduino.h>
+#include <BleKeyboard.h>
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
 
-// put function declarations here:
-int myFunction(int, int);
+
+void SummonAssist(); //Function Declaration
+
+BleKeyboard bleKeyboard("BLE Keyboard", "ESP32", 100);
 
 void setup() {
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //Disables burnout detector while powering the ESP32 over USB
   Serial.begin(115200);
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-  Serial.println(result);
+  Serial.println("Starting BLE work!");
+  bleKeyboard.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(bleKeyboard.isConnected()) { //If bluetooth is connected
+    //bleKeyboard.print("Hello world");
+    Serial.println("Sending Play/Pause media key...");
+    bleKeyboard.write(KEY_MEDIA_PLAY_PAUSE);
+    delay(1000);
+
+  }
+
+  Serial.println("Waiting 5 seconds...");
+  delay(5000);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+
+void SummonAssist(){ // Summon Google assistant (Only Works with Android)
+  bleKeyboard.press(KEY_LEFT_GUI);
+  delay(2000);
+  bleKeyboard.release(KEY_LEFT_GUI);
 }
